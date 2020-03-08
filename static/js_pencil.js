c596b06558182452ed32d809ecd67aae7007782c
Nelson Chen <crazysim@gmail.com>
2020-03-08 11:25:53 -0700

    Able to roughly draw but not save

--- static/js_pencil.js
+++ static/js_pencil.js
@@ -1,4 +1,8 @@
+const PxBrush = window['px-brush'].default;
+
 const canvas = document.getElementById('main-canvas');
+const pxBrush = new PxBrush(canvas);
+
 const ctx = canvas.getContext('2d');
 let currentSize = 10;
 let curZoom = 1;
@@ -11,16 +15,16 @@ let mode = 'brush';
 
 console.log(classes);
 
-classes.forEach((col,i)=>{
-    console.log(col,i);
-    $( "#pencils1" ).append( "<div data-class='pencil-"+col['id']+"' class='c-p-div btn-outline-secondary'><p class='c-p-text'>"+col['name']+"</p><div class='color-pencil' style='background:"+col['color']+"'></div></div>");
+classes.forEach((col, i) => {
+    console.log(col, i);
+    $("#pencils1").append("<div data-class='pencil-" + col['id'] + "' class='c-p-div btn-outline-secondary'><p class='c-p-text'>" + col['name'] + "</p><div class='color-pencil' style='background:" + col['color'] + "'></div></div>");
 });
 
 
-$(".c-p-div").click((evt)=>{
+$(".c-p-div").click((evt) => {
     console.log($(evt.target).data("class"));
-    $(".c-p-div").css({"border":"none","background":"#eee"});
-    $(evt.target).css({"border":"1px #333 solid", "background":"#aaa"});
+    $(".c-p-div").css({"border": "none", "background": "#eee"});
+    $(evt.target).css({"border": "1px #333 solid", "background": "#aaa"});
     curClass = parseInt($(evt.target).data("class").split('-')[1]);
 });
 
@@ -28,43 +32,53 @@ function getMousePos(canvas, evt, zoom) {
     const rect = canvas.getBoundingClientRect();
     // console.log(evt.clientX, evt.clientY, rect, evt);
     return {
-        x: (evt.clientX - rect.left)/zoom,
-        y: (evt.clientY - rect.top)/zoom
+        x: (evt.clientX - rect.left) / zoom,
+        y: (evt.clientY - rect.top) / zoom
         // x: evt.screenX - rect.left,
         // y: evt.screenY - rect.top
     };
-    
+
 }
 
-ctx.fillStyle = '#fff';
-ctx.fillRect(0,0,canvas.width, canvas.height);
+// Start off with Nothing
+ctx.fillStyle = '#ffffff';
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
 currentCanvasState = ctx.getImageData(0, 0, canvas.width, canvas.height);
+
 // ON MOUSE DOWN
 
 let isMouseDown = false;
+
 let curPath = [];
 let globalPaths = [];
 let overlayImg = null;
 
 function mousedown(canvas, evt) {
-    if (mode==='brush'){
-        isMouseDown=true;
-        var currentPosition = getMousePos(canvas, evt, curZoom);
-        curPath = [[currentPosition.x, currentPosition.y]];
-        ctx.moveTo(currentPosition.x, currentPosition.y);
-        ctx.beginPath();
-        ctx.lineWidth  = currentSize;
-        ctx.lineCap = "round";
-        ctx.strokeStyle = classes[curClass].color;
+    let currentPosition = getMousePos(canvas, evt, curZoom);
+
+    if (mode === 'brush') {
+        isMouseDown = true;
+        pxBrush.draw({
+            from: currentPosition,
+            to: currentPosition,
+            size: currentSize,
+            color: classes[curClass].color,
+        });
+        // curPath = [[currentPosition.x, currentPosition.y]];
+        // ctx.moveTo(currentPosition.x, currentPosition.y);
+        // ctx.beginPath();
+        // ctx.lineWidth  = currentSize;
+        // ctx.lineCap = "round";
+        // ctx.strokeStyle = classes[curClass].color;
+        // ctx.fillStyle = classes[curClass].color;
+    } else if (mode === 'fill') {
+        currentPosition = getMousePos(canvas, evt, curZoom);
         ctx.fillStyle = classes[curClass].color;
+        ctx.fillFlood(currentPosition.x, currentPosition.y, 10);
+
     }
-    else if (mode ==='fill'){
-        var currentPosition = getMousePos(canvas, evt, curZoom);
-        ctx.fillStyle = classes[curClass].color;
-        ctx.fillFlood(currentPosition.x, currentPosition.y,10);
-        
-    }
-    
+
 
 }
 
@@ -72,41 +86,47 @@ function mousedown(canvas, evt) {
 
 function mousemove(canvas, evt) {
 
-    if(isMouseDown){
+    if (isMouseDown) {
         const currentPosition = getMousePos(canvas, evt, curZoom);
-        ctx.lineTo(currentPosition.x, currentPosition.y);
-        ctx.stroke();
-        curPath.push([currentPosition.x, currentPosition.y]);
+                pxBrush.draw({
+            from: currentPosition,
+            to: currentPosition,
+            size: currentSize,
+            color: classes[curClass].color,
+        });
+        // ctx.lineTo(currentPosition.x, currentPosition.y);
+        // ctx.stroke();
+        // curPath.push([currentPosition.x, currentPosition.y]);
     }
 }
 
 // ON MOUSE UP
 
 function mouseup() {
-    if (mode==='brush'){
-        
-        isMouseDown=false;
-        globalPaths.push({path:curPath,className:curClass,color:classes[curClass].color, size:currentSize});
+    if (mode === 'brush') {
+
+        isMouseDown = false;
+        // globalPaths.push({path: curPath, className: curClass, color: classes[curClass].color, size: currentSize});
         drawAllPaths();
     }
 }
 
-function drawAllPaths(){
-    ctx.clearRect(0, 0, canvas.width,canvas.height);
+function drawAllPaths() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#fff';
-    ctx.fillRect(0,0,canvas.width, canvas.height);
-    if (overlayImg!==null){
-        ctx.drawImage(overlayImg,0,0);
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    if (overlayImg !== null) {
+        ctx.drawImage(overlayImg, 0, 0);
     }
-    globalPaths.forEach((pathObj)=>{
+    globalPaths.forEach((pathObj) => {
         const path = pathObj.path;
         const className = pathObj.className;
         const color = pathObj.color;
         const size = pathObj.size;
         ctx.beginPath();
-        ctx.moveTo(path[0][0],path[0][1]);
-        path.forEach((pt)=>{
-            ctx.lineTo(pt[0],pt[1]);    
+        ctx.moveTo(path[0][0], path[0][1]);
+        path.forEach((pt) => {
+            ctx.lineTo(pt[0], pt[1]);
         });
         // ctx.closePath();
         ctx.lineWidth = size;
@@ -117,28 +137,32 @@ function drawAllPaths(){
     });
     currentCanvasState = ctx.getImageData(0, 0, canvas.width, canvas.height);
     console.log(currentCanvasState);
-    
+
 }
 
 
-canvas.addEventListener('mousedown', function() {mousedown(canvas, event);});
-canvas.addEventListener('mousemove',function() {mousemove(canvas, event);});
-canvas.addEventListener('mouseup',mouseup);
+canvas.addEventListener('mousedown', function () {
+    mousedown(canvas, event);
+});
+canvas.addEventListener('mousemove', function () {
+    mousemove(canvas, event);
+});
+canvas.addEventListener('mouseup', mouseup);
 
 
-$('#undo').click(()=>{
+$('#undo').click(() => {
     globalPaths.pop();
     drawAllPaths();
 });
 
 
-$("#download").click((evt)=>{
+$("#download").click((evt) => {
     drawAllPaths();
     const image = canvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
     const link = document.createElement('a');
     link.download = img_name;
-  link.href = image;
-  link.click();
+    link.href = image;
+    link.click();
 });
 
 $("#save_to_disk").click((evt) => {
@@ -150,87 +174,86 @@ $("#save_to_disk").click((evt) => {
     });
 });
 
-$("#load").click((evt)=>{
+$("#load").click((evt) => {
     overlayImg = new Image();
-    overlayImg.src = '/masks/'+img_name + '?' + Date.now();
+    overlayImg.src = '/masks/' + img_name + '?' + Date.now();
     overlayImg.onload = drawAllPaths;
-    overlayImg.onerror = function(){
+    overlayImg.onerror = function () {
         overlayImg = null;
     }
 });
 
-$('#clear').click(()=>{
+$('#clear').click(() => {
     overlayImg = null;
     globalPaths = [];
     drawAllPaths();
 });
 
-$(document).on('input', '#myRange', function() {
+$(document).on('input', '#myRange', function () {
     // console.log($(this).val());
     currentSize = $(this).val();
     // console.log(currentSize);
     $("#brush-size").html(currentSize);
-    $(".color-pencil").each(function( index ) {
-        $( this ).css({"width":currentSize,"height":currentSize,"border-radius":currentSize/2});
-      });
+    $(".color-pencil").each(function (index) {
+        $(this).css({"width": currentSize, "height": currentSize, "border-radius": currentSize / 2});
+    });
 });
 
-$(document).on('input', '#myRangeOpacity', function() {
+$(document).on('input', '#myRangeOpacity', function () {
 
-    $("#main-canvas").css({"opacity":$(this).val()/10});
-    $("#overlay-opacity").html($(this).val()/10);
+    $("#main-canvas").css({"opacity": $(this).val() / 10});
+    $("#overlay-opacity").html($(this).val() / 10);
 });
 
 
-$(document).on('input', '#myRangeZoom', function() {
+$(document).on('input', '#myRangeZoom', function () {
     curZoom = $(this).val();
     $("#overlay-zoom").html(curZoom);
-    $("#main-canvas").css({"transform":"scale("+curZoom+")"});
-    $("#main-img").css({"transform":"scale("+curZoom+")"});
+    $("#main-canvas").css({"transform": "scale(" + curZoom + ")"});
+    $("#main-img").css({"transform": "scale(" + curZoom + ")"});
 
     // currentSize = $("#myRange").val()/curZoom;
     // console.log(currentSize);
 });
 
 
-$('#previous').click(()=>{
-    window.location.href = '/pencil?id='+(parseInt(img_id)-1)
+$('#previous').click(() => {
+    window.location.href = '/pencil?id=' + (parseInt(img_id) - 1)
 });
 
-$('#next').click(()=>{
-    window.location.href = '/pencil?id='+(parseInt(img_id)+1)
+$('#next').click(() => {
+    window.location.href = '/pencil?id=' + (parseInt(img_id) + 1)
 });
 
 
-
-$('#go-to-button').click(()=>{
+$('#go-to-button').click(() => {
     const togoto = $('#go-to').val();
-    window.location.href = '/pencil?id='+togoto;
+    window.location.href = '/pencil?id=' + togoto;
 });
 
-$('#brush').click((evt)=>{
+$('#brush').click((evt) => {
     mode = 'brush';
     $('.modes').removeClass("active");
     $(evt.target).addClass("active");
 
 });
 
-$('#fill').click((evt)=>{
+$('#fill').click((evt) => {
     mode = 'fill';
     $('.modes').removeClass("active");
     $(evt.target).addClass("active");
 });
 
 
-$('#commit').click(()=>{
-    
+$('#commit').click(() => {
+
     $.ajax({
-        url: "/hub-action/?imgfile="+img_name,
-        success: function(data){
+        url: "/hub-action/?imgfile=" + img_name,
+        success: function (data) {
             console.log(data);
         },
-      });
-      
-      
+    });
+
+
 });
 
